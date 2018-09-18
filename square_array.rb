1b2ca5659930f98bde3c785337f6aeb700a27918
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-18 13:22:01 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,6 @@
 def square_array(array)
   sqrt_arr = []
   array.each do | number |
-    Math.sqrt(number)
-    return sqrt_arr.push(number)
-  end
-end
-
-def square_array(array)
-  sqrt_arr = []
-  array.each do | number |
     sqrt_arr.push(number**2)
   end
   sqrt_arr
